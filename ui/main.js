2d3213b22010f9cb09c070f5ecb5f40be5d8bff3
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-13 20:55:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='hi this is javascript';
+var img=document.getElementById('madi');
+img.onclick = function(){
+    img.style.marginLeft='100px';
+    
+};
