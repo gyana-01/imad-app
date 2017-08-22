c874a7cb8d50ed187d0eec6545ee58bdb0cdbfee
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-22 12:38:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 var names=[];
-app.get('/submit-name' ,function(req,res){
+/*app.get('/submit-name' ,function(req,res){
   var name=req.query.name;
   names.push(name);
   res.send(JSON.stringify(names));
@@ -154,7 +154,7 @@ app.get('/articles/:articleName',function(req,res) {
        }
        }
     });
-});
+*/
 var Pool= new Pool(config);
 app.get('/test-db',function(req,res){
    //make a select request
