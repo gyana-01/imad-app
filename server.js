6d6b2bb9f81cca9f20f21d7fb8a2e747a83df505
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-22 12:36:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool=require('pg').Pool;
+var config ={
+    user:'gyanaprakashsahu1997',
+    database:'gyanaprakashsahu1997',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -102,7 +109,7 @@ var htmlTemplate= `<html>
         ${heading}
     </h3>
     <div>
-        ${date}
+        ${date.toDateString()}
     </div>
 <div>
     ${content}
@@ -124,11 +131,43 @@ app.get('/counter',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
-app.get('/:articleName',function(req,res) {
-var articleName = req.params.articleName;
-res.send(createTemplate(articles[articleName]));
+var names=[];
+app.get('/submit-name' ,function(req,res){
+  var name=req.query.name;
+  names.push(name);
+  res.send(JSON.stringify(names));
 });
-
+app.get('/articles/:articleName',function(req,res) {
+    pool.query("SELECT * FROM article WHERE tittle='" +req.params.articleName +"'", function(err,result){
+       if(err) {
+           res.status(500).send(err.toString());
+           
+       } else
+       {if(result.rows.length===0){
+           res.status(404).send('Article not found');
+           }
+           else
+           
+       {
+           var artcleData=result.rows[0];
+           res.send(CreateTemplate(articles[articleData]));)
+       }
+       }
+    });
+});
+var Pool= new Pool(config);
+app.get('/test-db',function(req,res){
+   //make a select request
+   //get a response
+   pool.query('SELECT * FROM test',function(err,result){
+       if(err)
+       {res.status(500).send(err.toString());
+   }
+   else
+   {res.send(JSON.stringify(result));
+   }
+   });
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
