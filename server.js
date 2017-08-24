03332a1f0bab7ebf490e7838007b8235417f846b
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-24 15:15:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -146,7 +146,8 @@ app.get('/articles/:articleName',function(req,res) {
        if(err) {
            res.status(500).send(err.toString());
            
-       } else
+       } 
+       else
        {if(result.rows.length===0){
            res.status(404).send('Article not found');
            }
