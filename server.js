fb9fecda6395055223a77cc9ef7e72119ed12b3d
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-30 20:59:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config ={
     user:'gyanaprakashsahu1997',
     database:'gyanaprakashsahu1997',
@@ -123,8 +124,16 @@ var htmlTemplate= `<html>
 `;
 return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+function hash(input,salt){
+    var hashed =crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return["pbkdf2","10000",salt, hashed.toString('hex')].join('$');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'this-is-some-random-string');
 });
 var counter=0;
 app.get('/counter',function(req,res){
