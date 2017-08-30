a9f67a3e8374fa7665d95e3043003eeba89c66f6
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-30 21:25:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,6 +134,7 @@ function hash(input,salt){
 }
 app.get('/hash/:input',function(req,res){
     var hashedString=hash(req.params.input,'this-is-some-random-string');
+    res.send(hashedString);
 });
 var counter=0;
 app.get('/counter',function(req,res){
