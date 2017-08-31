1f3f56807358193fafcfaabc20870ce817256a41
gyana-01 <gyanaprakashsahu1997@gmail.com>
2017-08-31 09:44:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -155,6 +155,35 @@ app.post('/create-user',function(req,res){
            }
     });
 });
+app.post('/login',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    
+    pool.query('SELECT * FROM  "user" WHERE username = $1',function(err,result){
+         if(err) {
+           res.status(500).send(err.toString());
+           
+       } 
+       else{
+           if(result.rows.length===0)
+           {
+               res.send(403).send('username/password is invalid');
+           }
+       else
+       {
+           var dbString=result.row[0].password;
+           var salt=dbString.split('$')[2];
+           var hashedPassword=hash(password,salt);
+           if(hashedPassword===dbString){
+               res.send('Credientials correct!');
+           }
+           else
+           {res.send(403).send('username/password is invalid');
+           }
+       }
+       }
+    });
+});
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
